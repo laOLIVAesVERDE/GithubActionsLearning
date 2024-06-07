981d6b7ea3c1e47053894f2bd2e73cc85399c3aa
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2024-06-07 17:53:38 +0900

    Revert "Release test"

--- app/src/main/java/com/laolivaesverde/githubactionslearning/MainActivity.kt
+++ app/src/main/java/com/laolivaesverde/githubactionslearning/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.laolivaesverde.githubactionslearning.ui.theme.GithubActionsLearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,4 +35,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GithubActionsLearningTheme {
+        Greeting("Android")
+    }
 }
