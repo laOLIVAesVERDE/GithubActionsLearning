dfe6eb22ed4aa0ce1efae3914e20fae7522fc263
JunyaMotohashi <junya.motohashi@dely.jp>
2024-06-15 23:58:56 +0900

    revise script

--- app/src/main/java/com/laolivaesverde/githubactionslearning/MainActivity.kt
+++ app/src/main/java/com/laolivaesverde/githubactionslearning/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello World!",
         modifier = modifier
     )
 }
